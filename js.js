321213fc741ed2cfce69c9d5a5070ceec1f6f6d3
cotrutatiberiu <32967624+cotrutatiberiu@users.noreply.github.com>
2017-11-19 23:00:12 +0200

    up

--- js.js
+++ js.js
@@ -1,8 +1,44 @@
-var ourRequest=new XMLHttpRequest();
-ourRequest.open("GET","https://learnwebcode.github.io/json-example/animals-1.json");
-ourRequest.onload=function(){
-    console.log(ourRequest.responseText);
-}
-ourRequest.send();
+var long;
+var lat;
 
-50c7c8e256d8581aadb566dd80b9ad8a
+var longElem=document.getElementById("lon");
+var latElem=document.getElementById("lat");
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showPosition(position) {
+  // x.innerHTML = "Latitude: " + position.coords.latitude +
+  // "<br>Longitude: " + position.coords.longitude;
+
+  long = position.coords.longitude;
+  lat = position.coords.latitude;
+  longElem.innerHTML=long;
+  latElem.innerHTML=lat;
+}
+
+getLocation();
+
+
+// var ourRequest = new XMLHttpRequest();
+// ourRequest.open(
+//   "GET",
+//   "http://api.openweathermap.org/data/2.5/weather?lat=" +
+//     lat +
+//     "&lon=" +
+//     long +
+//     "&APPID=50c7c8e256d8581aadb566dd80b9ad8a&units=metric"
+// );
+// ourRequest.onload = function() {
+//   var ourData = JSON.parse(ourRequest.responseText);
+//   document.write(ourData.coord.lon);
+// };
+// ourRequest.send();
+
+//50c7c8e256d8581aadb566dd80b9ad8a
+//http://api.openweathermap.org/data/2.5/weather?lat=20&lon=30&APPID=50c7c8e256d8581aadb566dd80b9ad8a&units=metric
