8cb805e743d1f1db9af65e1a85800796dee6fd40
cotrutatiberiu <32967624+cotrutatiberiu@users.noreply.github.com>
2017-11-20 22:38:42 +0200

    up

--- js.js
+++ js.js
@@ -1,8 +1,22 @@
-var long;
-var lat;
+var longitudeCoord;
+var latitudeCoord;
+var country;
+var city;
+var wind;
+var speed;
+var humidity;
+var pressure;
+var claudiness; //descrption
+var main;
+var temp;
+var img;
 
-var longElem=document.getElementById("lon");
-var latElem=document.getElementById("lat");
+var url;
+
+var firstHElem = document.getElementById("firstH");
+var secondHElem = document.getElementById("secondH");
+var pElem = document.getElementById("pElem");
+var imgElem = document.getElementById("imgE");
 
 function getLocation() {
   if (navigator.geolocation) {
@@ -13,32 +27,113 @@ function getLocation() {
 }
 
 function showPosition(position) {
-  // x.innerHTML = "Latitude: " + position.coords.latitude +
-  // "<br>Longitude: " + position.coords.longitude;
-
-  long = position.coords.longitude;
-  lat = position.coords.latitude;
-  longElem.innerHTML=long;
-  latElem.innerHTML=lat;
+  longitudeCoord = position.coords.longitude;
+  latitudeCoord = position.coords.latitude;
+  //Pass cordinates
+  getWeather(longitudeCoord, latitudeCoord);
 }
 
 getLocation();
 
+function getWeather(longitudeCoord, latitudeCoord) {
+  var ourRequest = new XMLHttpRequest();
+  ourRequest.open(
+    "GET",
+    "http://api.openweathermap.org/data/2.5/weather?lat=" +
+      latitudeCoord +
+      "&lon=" +
+      longitudeCoord +
+      "&APPID=50c7c8e256d8581aadb566dd80b9ad8a&units=metric"
+  );
+  ourRequest.onload = function() {
+    var ourData = JSON.parse(ourRequest.responseText);
+    main = ourData.weather[0].main;
+    city = ourData.name;
+    country = ourData.sys.country;
+    temp = ourData.main.temp;
+    humidity = ourData.main.humidity;
+    claudiness = ourData.weather[0].description;
+    pressure = ourData.main.pressure;
+    speed = ourData.wind.speed;
+    img = ourData.weather[0].icon;
 
-// var ourRequest = new XMLHttpRequest();
-// ourRequest.open(
-//   "GET",
-//   "http://api.openweathermap.org/data/2.5/weather?lat=" +
-//     lat +
-//     "&lon=" +
-//     long +
-//     "&APPID=50c7c8e256d8581aadb566dd80b9ad8a&units=metric"
-// );
-// ourRequest.onload = function() {
-//   var ourData = JSON.parse(ourRequest.responseText);
-//   document.write(ourData.coord.lon);
-// };
-// ourRequest.send();
+    print(
+      main,
+      city,
+      country,
+      temp,
+      humidity,
+      claudiness,
+      pressure,
+      speed,
+      longitudeCoord,
+      latitudeCoord
+    );
+  };
+  ourRequest.send();
+}
+
+function print(
+  main,
+  city,
+  country,
+  temp,
+  humidity,
+  claudiness,
+  pressure,
+  speed,
+  longitudeCoord,
+  latitudeCoord
+) {
+  firstHElem.innerHTML = "Weather in " + city + ", " + country;
+  url = setUrl(img);
+  imgElem.setAttribute("src", url);
+  secondHElem.innerHTML=temp + " &#8451";
+  pElem.innerHTML="Wind: "+ speed + " m/s<br></br>Cloudiness: "+ claudiness + "<br></br> Pressure : " + pressure +" hpa <br></br> Humidity: "+ humidity+"% <br></br> <em>Coordinates</em>: <br></br> Longitude: "+longitudeCoord+"<br></br> Latitude: "+latitudeCoord;
+}
+
+
+function setUrl(img){
+  switch(img){
+    case "01d":
+    return "http://openweathermap.org/img/w/01d.png";
+    case "01n":
+    return "http://openweathermap.org/img/w/01n.png";
+    case "02d":
+    return "http://openweathermap.org/img/w/02d.png";
+    case "02n":
+    return "http://openweathermap.org/img/w/02n.png";
+    case "03d":
+    return "http://openweathermap.org/img/w/03d.png";
+    case "03n":
+    return "http://openweathermap.org/img/w/03n.png";
+    case "04d":
+    return "http://openweathermap.org/img/w/04d.png";
+    case "04n":
+    return "http://openweathermap.org/img/w/04n.png";
+    case "09d":
+    return "http://openweathermap.org/img/w/09d.png";
+    case "09n":
+    return "http://openweathermap.org/img/w/09n.png";
+    case "10d":
+    return "http://openweathermap.org/img/w/10d.png";
+    case "10n":
+    return "http://openweathermap.org/img/w/10n.png";
+    case "11d":
+    return "http://openweathermap.org/img/w/11d.png";
+    case "11n":
+    return "http://openweathermap.org/img/w/11n.png";
+    case "13d":
+    return "http://openweathermap.org/img/w/13d.png";
+    case "13n":
+    return "http://openweathermap.org/img/w/13n.png";
+    case "50d":
+    return "http://openweathermap.org/img/w/50d.png";
+    case "50n":
+    return "http://openweathermap.org/img/w/50n.png";
+  }
+}
+//
 
 //50c7c8e256d8581aadb566dd80b9ad8a
 //http://api.openweathermap.org/data/2.5/weather?lat=20&lon=30&APPID=50c7c8e256d8581aadb566dd80b9ad8a&units=metric
